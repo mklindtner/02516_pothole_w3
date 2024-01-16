4738e83a7fe865215bccd0ec0c58d0e947612305
Benjamin <s236771@student.dtu.dk>
2024-01-16 10:24:54 +0100

    Change output layer to pothole problem (implementation by Mikkel)

--- src/pothole/models/resnet.py
+++ src/pothole/models/resnet.py
@@ -1,5 +1,12 @@
+import torch.nn as nn
 from torchvision.models import resnet50, ResNet50_Weights
 
 
-def get_resnet_model(model=resnet50, weights=ResNet50_Weights.IMAGENET1K_V1):
-    return model(weights=weights)
+def get_resnet_model(network=resnet50, weights=ResNet50_Weights.IMAGENET1K_V1):
+    model = network(weights=weights)
+
+    # Replace the final layer
+    n_features = model.fc.in_features
+    model.fc = nn.Linear(n_features, 1)
+
+    return model
